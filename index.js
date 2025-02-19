1394620f6310bccf1cf31cab408185ae06a0055e
mercan26 <mercan26@students.rowan.edu>
2025-02-19 10:20:36 -0500

    index.js

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
-/*trackerService.js
+var tracker = require("./tracker/trackerService.js");
 
-Provides basic Activity Tracker Functionality
-Activities:  Walk/Run
-Input:  Distance (miles)
-        Weight (lbs)
-        Time (hours)
-Output: Calories Burned
+var activities = [
+    {activity: "walking", weight: 150, distance: 3, time: 45},
+    {activity: "running", weight: 200, distance: 4, time: 40},
+    {activity: "running", weight: 175, distance: 5, time: 45},
+    {activity: "running", weight: 140, distance: 10, time: 240}
+];
 
- Formula Sources:
+for (let i = 0; i < activities.length; i++) {
+    let activity = activities[i];
 
- https://lovandy.com/wellness/physical/calories-burned-by-walking.html AND
- https://lovandy.com/wellness/physical/calories-burned-running.html
-*/
+    let timeInHours = activity.time / 60;
 
-//requires weight in kbls
-//
-function calcWalkCal(weight, distance){
-    return 0.3 * weight * distance;
+    let caloriesBurned;
+    let speed;
+
+    if (activity.activity === "walking") {
+        caloriesBurned = tracker.calcWalkCal(activity.weight, activity.distance);
+    } else if (activity.activity === "running") {
+        caloriesBurned = tracker.calcRunCal(activity.weight, activity.distance);
+    }
+
+    speed = tracker.calcSpeed(activity.distance, timeInHours);
+
+    console.log(`Activity: ${i + 1}`);
+    console.log(`${activity.activity}`);
+    console.log(`Weight: ${activity.weight} lbs`);
+    console.log(`Speed: ${speed.toFixed(2)} miles per hour`);
+    console.log(`Calories Burned: ${caloriesBurned.toFixed(0)}`);
+    console.log("***************************");
 }
-//requires weight in lbs, and distance in miles
-function calcRunCal(weight, distance){
-    return 0.63 * weight * distance;
-}
-//speed is whatever units the user wants to use
-function calcSpeed(distance, time){
-    return distance/time;
-}
 
-exports.calcWalkCal = calcWalkCal;
-exports.calcRunCal = calcRunCal;
-exports.calcSpeed = calcSpeed;
